9f1a0533709ee4b2ea61d7acbc521fc863034675
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2020-04-15 16:54:51 -0400

    Check point before continuing. This cod works toward an implementation where we loop though data rendinging React elements dynamically.

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -3,11 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Cody', age: 30},
+      {name: 'Rick', age: 33},
+      {name: 'Bob', age: 34}
+    ]
+  }
+
+  clickHandler = () => {
+    console.log('handler fired.');
+  }
+
   render() {
     return (
       <div className = "App">
         <h1>Hi, Im a React App</h1>
         <p>This is really working.</p>
+        <button onClick={this.clickHandler}>Click Me!</button>
         <Person name="Cody" age="30">I like photography.</Person>
         <Person name="Rick" age="32"/>
         <Person name="Bob" age="33"/>
--- react-sample/src/Person/Person.js
+++ react-sample/src/Person/Person.js
@@ -5,8 +5,7 @@ return (
     <div>
         <p>My name is { props.name }. I am {props.age} years old.</p>
         <p>{props.children}</p>
-    </div>
-    
+    </div>    
     );
 }
 
