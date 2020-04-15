71ddc607fbcd57bc8e6c653705c002d8a433309b
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2020-04-15 16:10:44 -0400

    Just moving some code around.

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
       <div className = "App">
         <h1>Hi, Im a React App</h1>
         <p>This is really working.</p>
-        <Person name="Cody" age="30"/>
-        <Person name="Rick" age="32">I like photography.</Person>
+        <Person name="Cody" age="30">I like photography.</Person>
+        <Person name="Rick" age="32"/>
         <Person name="Bob" age="33"/>
       </div>
     );
