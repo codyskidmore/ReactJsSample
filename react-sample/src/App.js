7ae989498d1185a463990b046aa765231af3227c
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2020-04-16 16:46:19 -0400

    Converted app component to a function with a useState hook.

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,32 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [
-      {name: 'Cody', age: 30},
-      {name: 'Rick', age: 33},
-      {name: 'Bob', age: 34}
-    ]
-  }
+const App = props => {
+    const [personsState, setPersonsState] = useState({
+      persons: [
+        {name: 'Cody', age: 30},
+        {name: 'Rick', age: 33},
+        {name: 'Bob', age: 34}
+      ],
+      otherState: 'some other value'
+    });
 
-  clickHandler = () => {
-    console.log('handler fired.');
-  }
+  const clickHandler = () => {
+    setPersonsState({
+      persons: [
+        {name: 'Dale', age: 30},
+        {name: 'Rick', age: 33}, 
+        {name: 'Bob', age: 34}
+      ],
+      otherState: personsState.otherState
+    });
+  };
 
-  render() {
-    return (
-      <div className = "App">
-        <h1>Hi, Im a React App</h1>
-        <p>This is really working.</p>
-        <button onClick={this.clickHandler}>Click Me!</button>
-        <Person name="Cody" age="30">I like photography.</Person>
-        <Person name="Rick" age="32"/>
-        <Person name="Bob" age="33"/>
-      </div>
-    );
-  }
+  return (
+    <div className = "App">
+      <h1>Hi, Im a React App</h1>
+      <p>This is working.</p>
+      <button onClick={clickHandler}>Click Me!</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>I like photography.</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
+    </div>
+  );
 }
 
 export default App;
